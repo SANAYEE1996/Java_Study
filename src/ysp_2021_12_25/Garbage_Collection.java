ed5490efb614eaa094a2a58be1a59677d27983cb
SANAYEE <dudtkd0219@gmail.com>
2021-12-25 23:29:29 +0900

    GC에 대한 유심한 고찰?

---
+++ src/ysp_2021_12_25/Garbage_Collection.java
@@ -0,0 +1,12 @@
+package ysp_2021_12_25;
+
+public class Garbage_Collection {
+
+	public static void main(String[] args) {
+		String u = "gogo";
+		u += " haha";
+		System.out.println(u);
+
+	}
+
+}
