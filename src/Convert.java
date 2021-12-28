c4cdffdaedd808afd3ffcb5de97f6fe628b5589c
SANAYEE <dudtkd0219@gmail.com>
2021-12-28 16:47:18 +0900

    오브젝트 이해하기

---
+++ src/Convert.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Convert {
+	public static List<String> convert(List<Object> input) {
+	 	List<String> output = new ArrayList<>();
+	 	String a = "";
+		for(Object i : input) {
+			a = i.getClass().getTypeName().toString().substring(10);
+			if(a.equals("String")) {
+				output.add(i.toString());
+			}
+			else {
+				gogo(output, (Object[]) i);
+			}
+		}
+		return output;
+	}
+	
+	public static void gogo(List<String> output, Object[] ob) {
+		String a = "";
+		for(Object i : ob) {
+			a = i.getClass().getTypeName().toString().substring(10);
+			if(a.equals("String")) {
+				output.add(i.toString());
+			}
+			else {
+				gogo(output, (Object[]) i);
+			}
+		}
+	}
+}
