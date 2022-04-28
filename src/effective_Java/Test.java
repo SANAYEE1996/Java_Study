3ca82741b69faf4847be609c158f854d4178d83e
SANAYEE <dudtkd0219@gmail.com>
2022-04-28 21:48:05 +0900

    틀린 예제

---
+++ src/effective_Java/Test.java
@@ -0,0 +1,16 @@
+package effective_Java;
+
+public class Test {
+	
+	public static boolean ValueOf(boolean b) {
+		return b? boolean.TRUE : boolean.FALSE; //틀린 코드
+	}
+
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+
+}
