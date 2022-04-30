45c7a4a7c9e0e9cf2f75d106c2c87075d91d4af2
SANAYEE <dudtkd0219@gmail.com>
2022-04-30 23:54:29 +0900

    틀린코드

--- src/effective_Java/Test.java
+++ src/effective_Java/Test.java
@@ -3,7 +3,7 @@ package effective_Java;
 public class Test {
 	
 	public static boolean ValueOf(boolean b) {
-		return b? boolean.TRUE : boolean.FALSE; //틀린 코드
+		return b;//? boolean.TRUE : boolean.FALSE; //틀린 코드
 	}
 
 	public static void main(String[] args) {
