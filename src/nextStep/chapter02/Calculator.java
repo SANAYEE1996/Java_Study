0af6bd6bfacf721324a4b65ee3df5b75039166d6
SANAYEE <dudtkd0219@gmail.com>
2022-06-09 00:36:01 +0900

    계산기 코드

--- src/nextStep/chapter02/Calculator.java
+++ src/nextStep/chapter02/Calculator.java
@@ -1,5 +1,24 @@
 package nextStep.chapter02;
 
 public class Calculator {
-
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public int minus(int a, int b) {
+		return a-b;
+	}
+	
+	public int multiple(int a, int b) {
+		return a*b;
+	}
+	
+	public int divide(int a, int b) {
+		return a/b;
+	}
+	
+	public int remain(int a, int b) {
+		return a%b;
+	}
+	
 }
