dc1ab2c0fa2e0e0f48046d977bd03881866116e7
SANAYEE <dudtkd0219@gmail.com>
2022-08-11 17:21:39 +0900

    .

--- src/cleanCode/chapter06/objectShape/Shape.java
+++ src/cleanCode/chapter06/objectShape/Shape.java
@@ -3,3 +3,4 @@ package cleanCode.chapter06.objectShape;
 public interface Shape {
 	public double area();
 }
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
