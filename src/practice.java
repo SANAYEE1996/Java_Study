73f4b0154b8fb43668d08715f1e1fede94d16740
SANAYEE <dudtkd0219@gmail.com>
2022-09-28 08:22:50 +0900

    .

--- src/practice.java
+++ src/practice.java
@@ -1,5 +1,10 @@
+import java.util.ResourceBundle;
+
+
 public class practice {
     public static void main(String[] args) {
-        
+    	ResourceBundle box = ResourceBundle.getBundle("locale_korean");
+    	
+    	
     }
 }
