74d46d20a94f57787332d006ae35f6cda7fae4dc
SANAYEE <dudtkd0219@gmail.com>
2022-09-30 13:50:28 +0900

    두 css 파일 있을 때 중복되는 태그 모으는 파일

--- src/FindSameTagCss.java
+++ src/FindSameTagCss.java
@@ -1,44 +1,68 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class FindSameTagCss {
-	public static void main(String[] args) throws IOException {
-        
-        BufferedReader classBr = new BufferedReader(new FileReader("C:/Users/imaxsoft-16/Desktop/classCss.txt"));
-        int count = 0;
+	
+	void readCssLine(BufferedReader br, ArrayList<String> tagList) throws IOException {
+		int count = 0;
         int index = 0;
         String beforeLine;
         while(true) {
-            String line = classBr.readLine();
+            String line = br.readLine();
             count++;
             if (line==null) break;  // 더 이상 읽을 라인이 없을 경우 while 문을 빠져나간다.
             index = line.length();
             beforeLine = line;
+            if(line.contains("@")) {
+            	continue;
+            }
             if(line.contains("{") && !line.contains("@")) {
             	index = line.indexOf("{");
             	if(!line.contains("}")) {
             		while(true) {
-	            		line = classBr.readLine();
+            			count++;
+	            		line = br.readLine();
 	            		if(line.contains("}")) {
 	            			break;
 	            		}
 	            	}
             	}
             }
-            System.out.println(beforeLine.substring(0,index));
+            beforeLine = beforeLine.substring(0, index).replaceAll("}", "").trim().replaceAll(",", "");
+            if(beforeLine.equals("")) {
+            	continue;
+            }
+            tagList.add(beforeLine);
         }
-        System.out.println("전체 줄 수 : " +count);
-        classBr.close();
         
+        br.close();
+        System.out.println("태그 수 : " +tagList.size());
+	}
+	
+	
+	
+	public static void main(String[] args) throws IOException {
+        
+        BufferedReader classBr = new BufferedReader(new FileReader("C:/Users/imaxsoft-16/Desktop/classCss.txt"));
         BufferedReader adminBr = new BufferedReader(new FileReader("C:/Users/imaxsoft-16/Desktop/adminCss.txt"));
+        FindSameTagCss s = new FindSameTagCss();
+        ArrayList<String> classTagList = new ArrayList<>();
+        ArrayList<String> adminTagList = new ArrayList<>();
+        s.readCssLine(classBr,classTagList);
+        s.readCssLine(adminBr,adminTagList);
         
-        while(true) {
-            String line = adminBr.readLine();
-            count++;
-            if (line==null) break;  // 더 이상 읽을 라인이 없을 경우 while 문을 빠져나간다.
+        ArrayList<String> duplicateList = new ArrayList<>();
+        for(String str : classTagList) {
+        	if(adminTagList.contains(str) && !duplicateList.contains(str)) {
+        		duplicateList.add(str);
+        	}
         }
-        System.out.println("전체 줄 수 : " +count);
-        adminBr.close();
+        for(String str : duplicateList) {
+        	System.out.println(str);
+        }
+        
+        
 	}
 }
