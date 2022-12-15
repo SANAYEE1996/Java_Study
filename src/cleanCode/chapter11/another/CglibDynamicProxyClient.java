2cd865e6eeaddf1c592113176ec77ce8afde6ed4
SANAYEE1996 <dudtkd0219@gmail.com>
2022-12-15 14:40:04 +0900

    .

--- src/cleanCode/chapter11/another/CglibDynamicProxyClient.java
+++ src/cleanCode/chapter11/another/CglibDynamicProxyClient.java
@@ -1,6 +1,6 @@
 package cleanCode.chapter11.another;
 
-import java.lang.reflect.Method;
+//import java.lang.reflect.Method;
 
 public class CglibDynamicProxyClient {
 	public static void main(String[] args) {
@@ -10,20 +10,20 @@ public class CglibDynamicProxyClient {
 
     public void run(String name) {
 
-        MethodInterceptor methodInterceptor = new MethodInterceptor() {
-            final TargetObject targetObject = new TargetObject();
-
-            @Override
-            public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
-                System.out.println("Before Proxy");
-                Object invoke = method.invoke(targetObject, args);
-                System.out.println("After Proxy");
-                return invoke;
-            }
-        };
-
-        TargetObject targetObject = (TargetObject) Enhancer.create(TargetObject.class, methodInterceptor);
-
-        targetObject.someMethod(name);
+//        MethodInterceptor methodInterceptor = new MethodInterceptor() {
+//            final TargetObject targetObject = new TargetObject();
+//
+//            @Override
+//            public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+//                System.out.println("Before Proxy");
+//                Object invoke = method.invoke(targetObject, args);
+//                System.out.println("After Proxy");
+//                return invoke;
+//            }
+//        };
+//
+//        TargetObject targetObject = (TargetObject) Enhancer.create(TargetObject.class, methodInterceptor);
+//
+//        targetObject.someMethod(name);
     }
 }
